d1b0adb125f226c6435dadbe51fce4eb04c2511c
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2022-05-16 16:52:39 +0200

    Création branche US6

--- frontend/screens/DashboardScreen.js
+++ frontend/screens/DashboardScreen.js
@@ -1,10 +1,19 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Switch } from "react-native";
 
 export default function DashboardScreen(props) {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
   return (
     <View style={styles.container}>
-      <Text>Dashboard</Text>
+      <Switch
+        trackColor={{ false: "#767577", true: "#81b0ff" }}
+        thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
+      <Text>Aidez nous à personnaliser le prfil de votre enfant</Text>
       <Text>Personnaliser les infos</Text>
       <Text>Stat perso</Text>
     </View>
