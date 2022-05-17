af7fd41c0ccf1b823b1c23a1912f82bd6db5bf74
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2022-05-17 14:07:44 +0200

    Mise en page de la partie personnalisation dans l'onglet Dashboard

--- frontend/screens/DashboardScreen.js
+++ frontend/screens/DashboardScreen.js
@@ -1,20 +1,78 @@
 import React, { useState } from "react";
 import {
   StyleSheet,
-  Text,
   View,
-  useWindowDimensions,
   Dimensions,
   StatusBar,
+  ScrollView,
+  TouchableOpacity,
+  TextInput,
 } from "react-native";
+import { Text, Card } from "@rneui/themed";
 import { TabView, SceneMap, TabBar } from "react-native-tab-view";
 
+// Onglet personnalisation des notions
 const FirstRoute = () => (
-  <View style={[styles.scene, { backgroundColor: "white" }]} />
+  <View style={[styles.scene, { backgroundColor: "white" }]}>
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.fonts} h4>
+          Cockpit de Gabrielle
+        </Text>
+        <Text style={styles.fonts} h5>
+          Aidez nous à personnaliser le programme de votre enfant !
+        </Text>
+        <View style={styles.card}>
+          <Card.Title style={styles.title}>Français</Card.Title>
+          <View style={styles.buttonDisplay}>
+            <TouchableOpacity>
+              <Text style={styles.button}>Orthographe</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Text style={styles.button}>Grammaire</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Text style={styles.button}>Dictée</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Text style={styles.button}>Conjugaison</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Text style={styles.button}>Vocabulaire</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <View style={styles.card}>
+          <Card.Title style={styles.title}>Maths</Card.Title>
+          <View style={styles.buttonDisplay}>
+            <TouchableOpacity>
+              <Text style={styles.button}>Géométrie</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Text style={styles.button}>Calcul</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
+  </View>
 );
 
+// Onglet Statistique
 const SecondRoute = () => (
-  <View style={[styles.scene, { backgroundColor: "white" }]} />
+  <View style={[styles.scene, { backgroundColor: "white" }]}>
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.fonts} h4>
+          Cockpit de Gabrielle
+        </Text>
+        <Text style={styles.fonts} h5>
+          Suivez les progrès de votre enfant
+        </Text>
+      </View>
+    </ScrollView>
+  </View>
 );
 
 const initialLayout = { width: Dimensions.get("window").width };
@@ -56,7 +114,42 @@ const styles = StyleSheet.create({
   container: {
     marginTop: StatusBar.currentHeight,
   },
+  card: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
+    borderRadius: 15,
+    shadowOffset: { width: 5, height: 5},
+    shadowOpacity: 1,
+    shadowRadius: 8,
+    elevation: 8,
+    paddingLeft: 16,
+    paddingRight: 14,
+    marginTop: 15,
+    marginBottom: 20,
+    marginLeft: 16,
+    marginRight: 16,
+  },
+  buttonDisplay: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  button: {
+    color: "white",
+    backgroundColor: "#FABE6D",
+    padding: 8,
+    marginRight: 10,
+    marginBottom: 10,
+  },
   scene: {
     flex: 1,
+  },
+  fonts: {
+    marginTop: 10,
+    marginBottom: 8,
+    textAlign: "center",
+  },
+  title: {
+    marginTop: 10,
   },
 });
